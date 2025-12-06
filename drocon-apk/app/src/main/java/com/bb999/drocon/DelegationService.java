0ceecd5a23070244ed683800ccae13af4da5d201
のらねこび <noranekob@noranekobinoMacBook-Air.local>
2025-12-06 21:43:19 +0900

    apk作成＆gitignore作成

---
+++ drocon-apk/app/src/main/java/com/bb999/drocon/DelegationService.java
@@ -0,0 +1,14 @@
+package com.bb999.drocon;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+

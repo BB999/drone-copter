313be7014d637f0edbd4e36bbf2e4e9708a65a1c
のらねこび <noranekob@noranekobinoMacBook-Air.local>
2025-12-10 17:53:44 +0900

    feat: プライバシーポリシーページを追加

---
+++ drocon-apk-meta/build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /Users/noranekob/cursor/drone-copter/drocon-apk-meta
+/opt/homebrew/opt/node@20/bin/node /Users/noranekob/cursor/drone-copter/meta-bubblewrap/bubblewrap/packages/cli/bin/bubblewrap.js build

ac55091e0a5708cfca3b10bf5e768505313f4941
のらねこび <noranekob@noranekobinoMacBook-Air.local>
2025-12-11 16:00:55 +0900

    assetlinks.jsonを配置

--- drocon-apk-meta/run-bubblewrap.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /Users/noranekob/cursor/drone-copter/drocon-apk-meta
-/opt/homebrew/opt/node@20/bin/node /Users/noranekob/cursor/drone-copter/meta-bubblewrap/bubblewrap/packages/cli/bin/bubblewrap.js init --manifest=https://bb999.github.io/drone-copter/manifest.webmanifest --metaquest
